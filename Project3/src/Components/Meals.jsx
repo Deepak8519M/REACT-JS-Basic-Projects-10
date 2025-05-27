c9b1ee3d824b00933ccd10c8500cf5f0711b2495
Deepak Mallareddy <deepak8519m@gmail.com>
2025-05-27 08:59:56 +0530

    project3 end push

--- Project3/src/Components/Meals.jsx
+++ Project3/src/Components/Meals.jsx
@@ -1,11 +1,40 @@
 import React from "react";
 import "../style.css";
-import { useStat } from "react";
+import { useState, useEffect } from "react";
+import axios from "axios";
 
 function Meals() {
+  const [items, setItems] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get("https://www.themealdb.com/api/json/v1/1/search.php?s=")
+      .then((res) => {
+        // console.log(res.data.meals);
+        setItems(res.data.meals);
+      })
+      .catch((err) => console.log(err));
+  }, []);
+
+  const itemsList = items.map(({ strMeal, strMealThumb, idMeal }) => {
+    return (
+      <section className="card">
+        <img src={strMealThumb} alt={strMeal} />
+        <section className="content">
+          <p>{strMeal}</p>
+          <p>{idMeal}</p>
+        </section>
+      </section>
+    );
+  });
+
   return (
-    <div>
-      <h1>Hello</h1>
+    <div className="items-container">
+      {itemsList.length > 0 ? (
+        itemsList
+      ) : (
+        <p className="loading">Loading... Please Wait</p>
+      )}
     </div>
   );
 }
