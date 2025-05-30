ea44886906225a505d4f62f03995b7e56391b3f2
Deepak Mallareddy <deepak8519m@gmail.com>
2025-05-30 23:43:47 +0530

    next commit

--- Project6/src/HiddenSearchBar.jsx
+++ Project6/src/HiddenSearchBar.jsx
@@ -1,10 +1,32 @@
 import React from "react";
 import "./style.css";
+import { FaSearch } from "react-icons/fa";
+
+import { useState } from "react";
 function HiddenSearchBar() {
+  const [showInput, setShowInput] = useState(false);
+  const [bgColor, setBgColor] = useState("white");
+
+  const handleClick = (e) => {
+    setBgColor("#1a1a1a");
+
+    if (e.target.className === "container") {
+      setShowInput(false);
+      setBgColor("white");
+    }
+  };
   return (
-    <div>
-      <h1>hello</h1>
-    </div>
+    <section
+      className="container"
+      style={{ backgroundColor: bgColor }}
+      onClick={handleClick}
+    >
+      {showInput ? (
+        <input type="text" placeholder="Search..." />
+      ) : (
+        <FaSearch onClick={() => setShowInput(true)} />
+      )}
+    </section>
   );
 }
 
