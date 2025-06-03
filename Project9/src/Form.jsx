87a389438ed9c946730d0b92e04159047cb40120
Deepak Mallareddy <deepak8519m@gmail.com>
2025-06-03 10:37:37 +0530

    usestates added

--- Project9/src/Form.jsx
+++ Project9/src/Form.jsx
@@ -1,6 +1,23 @@
 import React from "react";
+import "./style.css";
+import { useState } from "react";
 
 function Form() {
+  const [userName, setUserName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+
+  const [errorUserName, setErrorUserName] = useState("");
+  const [errorEmail, setErrorEmail] = useState("");
+  const [errorPassword, setErrorPassword] = useState("");
+  const [errorConfirmPassword, setErrorConfirmPassword] = useState("");
+
+  const [userColor, setUserColor] = useState("");
+  const [emailColor, setEmailColor] = useState("");
+  const [passwordColor, setPasswordColor] = useState("");
+  const [confirmPasswordColor, setConfirmPasswordColor] = useState("");
+
   return <div></div>;
 }
 
